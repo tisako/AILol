313d062695202b15ebf5b6428cddab5a7605948a
koorets1 <Welcome01@>
2019-01-28 15:19:42 +0100

    #1: fix minor test bug for screencapture

--- tests/input_capture/test_screenCapture.py
+++ tests/input_capture/test_screenCapture.py
@@ -11,12 +11,12 @@ from tests import FileHelper
 class TestScreenCapture(TestCase):
     def setUp(self):
 
-        current_dir = FileHelper.get_current_dir(inspect.currentframe())
+        self.current_dir = FileHelper.get_current_dir(inspect.currentframe())
         self.capture = ScreenCapture()
-        self.capture.show_screen_capture(np.load(current_dir + '/ImageToShowScreenCapture.npy')[0])
+        self.capture.show_screen_capture(np.load(self.current_dir + '/ImageToShowScreenCapture.npy')[0])
         time.sleep(0.5)
 
     def test_capture_center_of_shown_screen(self):
-        test_img = np.load('TestDataScreenCapture.npy')
+        test_img = np.load(self.current_dir + '/TestDataScreenCapture.npy')
         input_image = self.capture.get_screen_image(0.4, 0.6, 0.4, 0.6)
         self.assertFalse((np.array(input_image) - test_img).any(), 'An element is different in input ' + input_image.__str__() + ' and output' + test_img.__str__())
