f865817b4069db6f34c2dd98caf559956d6ec2b6
koorets1 <Welcome01@>
2019-01-28 14:55:25 +0100

    #1: removed .idea folder added test for screencapture

--- tests/input_capture/test_screenCapture.py
+++ tests/input_capture/test_screenCapture.py
@@ -1,9 +1,18 @@
+import time
 from unittest import TestCase
+
+import numpy as np
+
+from src.input_capture.ScreenCapture import ScreenCapture
 
 
 class TestScreenCapture(TestCase):
-    def test_get_screen_image(self):
-        print('hoi')
+    def setUp(self):
+        self.capture = ScreenCapture()
+        self.capture.show_screen_capture(np.load('ImageToShowScreenCapture.npy')[0])
+        time.sleep(0.5)
 
-    def test_show_screen_capture(self):
-        print('hoi')
+    def test_capture_center_of_shown_screen(self):
+        test_img = np.load('TestDataScreenCapture.npy')
+        input_image = self.capture.get_screen_image(0.4, 0.6, 0.4, 0.6)
+        self.assertFalse((np.array(input_image) - test_img).any(), 'An element is different in input ' + input_image.__str__() + ' and output' + test_img.__str__())
