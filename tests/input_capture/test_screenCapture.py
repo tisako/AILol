4e7ff36329dae835f28c8fa6c0a5a74c99ea5135
koorets1 <Welcome01@>
2019-02-04 15:24:33 +0100

    #6: try fix the ci error

--- tests/input_capture/test_screenCapture.py
+++ tests/input_capture/test_screenCapture.py
@@ -1,22 +1,18 @@
-import time
 from unittest import TestCase
 
-import numpy as np
 import numpy.testing as npt
-
+from testfixtures import mock
 
 from src.input_capture.ScreenCapture import ScreenCapture
-from tests import FileHelper
 
 
 class TestScreenCapture(TestCase):
     def setUp(self):
-        self.current_dir = FileHelper.get_current_dir()
         self.capture = ScreenCapture()
-        self.capture.show_screen_capture(np.load(self.current_dir + '/ImageToShowScreenCapture.npy')[0])
-        time.sleep(0.5)
 
-    def test_capture_center_of_shown_screen(self):
-        test_img = np.load(self.current_dir + '/TestDataScreenCapture.npy')
+    @mock.patch('PIL.ImageGrab.grab')
+    def test_capture_center_of_shown_screen(self, mock_image_grab):
+        mock_image_grab.return_value = [1, 2, 3]
         input_image = self.capture.get_screen_image_relative(0.4, 0.6, 0.4, 0.6)
-        npt.assert_array_equal([input_image], test_img)
+        npt.assert_array_equal(input_image, [1, 2, 3])
+        mock_image_grab.assert_called_once_with(bbox=(768.0, 432.0, 1152.0, 648.0))
