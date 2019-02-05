da9443e4b299b127783cc62c85a4d41c1df0cc08
koorets1 <Welcome01@>
2019-02-05 18:16:44 +0100

    #6: module name's following pep8

--- src/main.py
+++ src/main.py
@@ -1,32 +1,9 @@
 from tkinter import Tk
 
-import cv2
-import time
-
-from src.frontend.TrainDataCollector import TrainDataCollector
-from src.input_capture.ScreenCapture import ScreenCapture
+from src.frontend.train_data_collector import TrainDataCollector
 
 root = Tk()
 root.geometry("500x500")
 app = TrainDataCollector(root)
 
 root.mainloop()
-
-# last_time = time.time()
-# stopTime = time.time()
-# timeout_time = 0
-# show_picture = True
-# capture = ScreenCapture()
-#
-# while True:
-#     print_screen = capture.get_screen_image()
-#     print('loop took {} seconds'.format(time.time() - last_time))
-#     last_time = time.time()
-#     if show_picture:
-#         capture.show_screen_capture(print_screen)
-#     if 0 < timeout_time < time.time() - stopTime or cv2.waitKey(25) & 0xFF == ord('q'):
-#         break
-#
-# while True:
-#     if cv2.waitKey(25) & 0xFF == ord('q'):
-#         break
