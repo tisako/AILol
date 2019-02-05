ac8e5f78a74d28e6f30c236f50ceadb23b48bd6c
koorets1 <Welcome01@>
2019-02-05 17:37:16 +0100

    #6: fix screen size files

--- src/input_capture/ScreenCapture.py
+++ src/input_capture/ScreenCapture.py
@@ -33,7 +33,7 @@ class ScreenCapture:
         grab = ImageGrab.grab(bbox=(self.width * start_width_proportion, self.height * start_height_proportion, self.width * end_width_proportion, self.height * end_height_proportion))
         return np.array(grab)
 
-    def get_screen_image(self, position=(0, 0), size=(get_screen_width.__func__(), get_screen_height.__func__())):
+    def get_screen_image(self, position, size):
         if position[0]+size[0] > self.width or position[1]+size[1] > self.height:
             raise ValueError('total width or heigth is bigger than screensize ' + repr(self.width) + 'x' + repr(self.height))
         return np.array(ImageGrab.grab(bbox=(position[0], position[1], position[0] + size[0], position[1] + size[1])))
