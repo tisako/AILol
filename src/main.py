23da0500a6acfb93cfd05bfddc18a73e4b063eb0
koorets1 <Welcome01@>
2019-02-06 11:22:12 +0100

    #6: remove main

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-
-from src.frontend.train_data_collector import TrainDataCollector
-
-root = Tk()
-root.geometry("500x500")
-app = TrainDataCollector(root)
-
-root.mainloop()
