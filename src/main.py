318470496c76f176937b29a1b06e6a2ba8143636
koorets1 <Welcome01@>
2019-02-06 11:22:42 +0100

    #6: add main

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk
+
+from src.frontend.train_data_collector import TrainDataCollector
+
+root = Tk()
+root.geometry("500x500")
+app = TrainDataCollector(root)
+
+root.mainloop()
